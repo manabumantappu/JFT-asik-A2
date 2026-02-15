d62ada3ffde65f2be48930327be5988e69c20738
manabumantappu <azishachigo@gmail.com>
2026-02-16 06:24:52 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,19 @@
+const vocabData = [
+{kanji:"行く", hiragana:"いく", arti:"Pergi"},
+{kanji:"仕事", hiragana:"しごと", arti:"Pekerjaan"}
+];
+
+const questionData = [
+{
+question:"Arti 仕事 adalah...",
+options:["Sekolah","Rumah","Pekerjaan","Libur"],
+answer:2,
+explanation:"仕事 berarti pekerjaan."
+},
+{
+question:"Arti 行く adalah...",
+options:["Pergi","Datang","Makan","Minum"],
+answer:0,
+explanation:"行く berarti pergi."
+}
+];
