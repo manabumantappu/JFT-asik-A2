71697061818f91053b5d18b55b761e2d21515cfb
manabumantappu <azishachigo@gmail.com>
2026-02-16 07:44:29 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,27 @@
+// js/firebase.js
+
 import { initializeApp } from 
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 
 import { getAuth } from 
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 import { getFirestore } from 
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBLKv-oOkCSrpCWUYL91zzjxlPFpV1H5U4",
+  authDomain: "jft-asik-a2.firebaseapp.com",
+  projectId: "jft-asik-a2",
+  storageBucket: "jft-asik-a2.firebasestorage.app",
+  messagingSenderId: "556612903099",
+  appId: "1:556612903099:web:a696aabd84a127b73b099e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export supaya bisa dipakai di file lain
 export const auth = getAuth(app);
 export const db = getFirestore(app);
