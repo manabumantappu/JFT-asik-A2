ffec3d001d2168bb73612fe0986d4f098da19a56
manabumantappu <azishachigo@gmail.com>
2026-02-16 09:25:23 +0700

    Update vocabService.js

--- js/services/vocabService.js
+++ js/services/vocabService.js
@@ -1,8 +1,31 @@
 import { db } from "../firebase.js";
-import { collection, getDocs } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  collection,
+  getDocs,
+  query,
+  where
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
+// Ambil semua vocab
 export async function getVocab() {
   const snapshot = await getDocs(collection(db, "vocab"));
-  return snapshot.docs.map(doc => doc.data());
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
+}
+
+// Filter berdasarkan kategori
+export async function getVocabByCategory(category) {
+  const q = query(
+    collection(db, "vocab"),
+    where("kategori", "==", category)
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
 }
