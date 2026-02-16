11eb974554e218225b7087ef117475c2a39c489e
manabumantappu <azishachigo@gmail.com>
2026-02-16 09:31:42 +0700

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,9 +1,9 @@
 import { auth } from "./firebase.js";
 import { onAuthStateChanged } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
   if (!user) {
-    location.href = "login.html";
+    window.location.href = "login.html";
   }
 });
