47e10bc9de5b4b6b46b7274a66f94877e3e5ae66
manabumantappu <azishachigo@gmail.com>
2026-02-16 15:03:06 +0700

    Create katakana.js

---
+++ js/data/katakana.js
@@ -0,0 +1,13 @@
+export const katakanaData = [
+  { char: "ア", romaji: "a" },
+  { char: "イ", romaji: "i" },
+  { char: "ウ", romaji: "u" },
+  { char: "エ", romaji: "e" },
+  { char: "オ", romaji: "o" },
+
+  { char: "カ", romaji: "ka" },
+  { char: "キ", romaji: "ki" },
+  { char: "ク", romaji: "ku" },
+  { char: "ケ", romaji: "ke" },
+  { char: "コ", romaji: "ko" }
+];
