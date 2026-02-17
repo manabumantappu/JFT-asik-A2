3eb425be1e4e74252801032bf7417889343bc1ed
manabumantappu <azishachigo@gmail.com>
2026-02-17 08:59:51 +0700

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -7,3 +7,29 @@ onAuthStateChanged(auth, (user) => {
    window.location.href = "login.html";
   }
 });
+import { doc, getDoc } from
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { db, auth } from "./firebase.js";
+import { signOut } from
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+auth.onAuthStateChanged(async (user) => {
+
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const snap = await getDoc(doc(db, "users", user.uid));
+
+  if (snap.exists()) {
+    const data = snap.data();
+
+    if (data.disabled === true) {
+      alert("Akun Anda dinonaktifkan oleh Admin.");
+      await signOut(auth);
+      window.location.href = "login.html";
+    }
+  }
+
+});
